2bbf3b19d9f6ab9223f8323236c1f0747d1aeba4
Niels Garve <niels.garve@yahoo.de>
2013-03-21 12:53:50 +0100

    Draggers

--- cg2/cg2-a01/circle.js
+++ cg2/cg2-a01/circle.js
@@ -1,18 +1,22 @@
 define(["util", "vec2", "scene", "point_dragger"],
-    (function () {
+    (function (Util, vec2, Scene, PointDragger) {
         "use strict";
 
-        var Circle = function (point, radius) {
+        var Circle = function (point, radius, circleStyle) {
             console.log("creating circle with: ");
 
-            var _circle = this;
-            this.point = point || [10, 10];
-            this.radius = radius || 5;
+            this.point = point || [50, 50];
+            this.radius = radius || 25;
+            this.circleStyle = circleStyle || {width: "2", color: "#0000AA"};
         };
 
         Circle.prototype.draw = function (context) {
             context.beginPath();
             context.arc(this.point[0], this.point[1], this.radius, 0, 360, false);
+
+            context.lineWidth = this.circleStyle.width;
+            context.strokeStyle = this.circleStyle.color;
+
             context.stroke();
         };
 
@@ -21,7 +25,21 @@ define(["util", "vec2", "scene", "point_dragger"],
         };
 
         Circle.prototype.createDraggers = function () {
+            var draggers = [],
+                draggerStyle = {radius: 4, color: this.circleStyle.color, width: 0, fill: true},
+                _circle = this,
 
+                getPoint = function () {
+                    return _circle.point;
+                },
+
+                setPoint = function (dragEvent) {
+                    _circle.point = dragEvent.position;
+                };
+
+            draggers.push(new PointDragger(getPoint, setPoint, draggerStyle));
+
+            return draggers;
         };
 
         return Circle;
