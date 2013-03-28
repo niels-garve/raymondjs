5d3d227d00d163aa901b763444ebbea868a43f8e
Niels Garve <niels.garve@yahoo.de>
2013-03-28 15:31:49 +0100

    webcl-tut kickoff

---
+++ webcl-tut/js/main.js
@@ -0,0 +1,38 @@
+requirejs.config({
+    paths: {
+        "jquery": [
+            'http://code.jquery.com/jquery-1.9.1.min',
+            'lib/jquery-1.9.1.min'],
+
+        "underscore": "lib/underscore-min",
+
+        "parallel": "lib/parallel.min",
+
+        "bootstrap": "lib/bootstrap.min"
+    },
+
+    shim: {
+        "jquery": {
+            exports: "$"
+        },
+
+        "underscore": {
+            exports: "_"
+        },
+
+        "parallel": {
+            deps: ["underscore"],
+            exports: "Parallel"
+        },
+
+        "bootstrap": {
+            deps: ["jquery"]
+        }
+    }
+});
+
+define(["jquery", "underscore", "parallel", "bootstrap"],
+    (function ($, _, Parallel) {
+
+    })
+);
