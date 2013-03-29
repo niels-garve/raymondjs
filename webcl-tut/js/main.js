9b21ab4c206ba9ce926dd258db9b9e381834e86b
Niels Garve <niels.garve@yahoo.de>
2013-03-29 15:23:41 +0100

    Lesson 3: Meine erste Vektor-Addition!

--- webcl-tut/js/main.js
+++ webcl-tut/js/main.js
@@ -36,6 +36,19 @@ define(
     (function ($, _, Parallel, Utils) {
         "use strict";
 
-        Utils.webClInfo();
+        // Generate input vectors
+        var vectorLength = 30,
+            UIvector1 = new Uint32Array(vectorLength),
+            UIvector2 = new Uint32Array(vectorLength),
+            i;
+
+        for (i = 0; i < vectorLength; i += 1) {
+            UIvector1[i] = Math.floor(Math.random() * 100); // Random number 0..99
+            UIvector2[i] = Math.floor(Math.random() * 100); // Random number 0..99
+        }
+
+        log(UIvector1);
+        log(UIvector2);
+        log(Utils.CL_vectorAdd(UIvector1, UIvector2, vectorLength));
     })
 );
--- webcl-tut/js/utils.js
+++ webcl-tut/js/utils.js
@@ -22,15 +22,120 @@ define(
                     // available on a platform.
                 }
 
-                console.log(devices);
+                log(devices);
 
             } catch (e) {
                 alert("Unfortunately platform or device inquiry failed.");
             }
         }
 
+        /**
+         * Returns the kernel source code.
+         * @param id
+         * @return {*}
+         */
+        function loadKernel(id) {
+            var kernelElement = document.getElementById(id), // TODO vermutlich ist jQuery zu langsam?
+                kernelSource = kernelElement.text;
+
+            if (kernelElement.src != "") {
+                var mHttpReq = new XMLHttpRequest();
+                mHttpReq.open("GET", kernelElement.src, false);
+                mHttpReq.send(null);
+                kernelSource = mHttpReq.responseText;
+            }
+            return kernelSource;
+        }
+
+        /**
+         * Setup WebCL context using the default device of the first platform
+         * @return IWebCLContext
+         */
+        function createContext() {
+            var platforms = WebCL.getPlatformIDs();
+
+            return WebCL.createContextFromType([WebCL.CL_CONTEXT_PLATFORM, platforms[0]],
+                WebCL.CL_DEVICE_TYPE_DEFAULT);
+        }
+
+        /**
+         *
+         * @param UIvec1
+         * @param UIvec2
+         * @param vectorLength
+         * @return {*|null|String}
+         * @constructor
+         */
+        function CL_vectorAdd(UIvec1, UIvec2, vectorLength) {
+            var program = null,
+                devices = null;
+
+            try {
+                if (window.WebCL === undefined) {
+                    alert("Unfortunately your system does not support WebCL. " +
+                        "Make sure that you have both the OpenCL driver " +
+                        "and the WebCL browser extension installed.");
+                    return null;
+                }
+
+                log("Vector length = " + vectorLength);
+
+                // Create Context, reserve buffers & create and build program for the first device
+                var ctx = createContext(),
+                    bufSize = vectorLength * 4, // size in bytes
+                    bufIn1 = ctx.createBuffer(WebCL.CL_MEM_READ_ONLY, bufSize),
+                    bufIn2 = ctx.createBuffer(WebCL.CL_MEM_READ_ONLY, bufSize),
+                    bufOut = ctx.createBuffer(WebCL.CL_MEM_WRITE_ONLY, bufSize),
+                    kernelSrc = loadKernel("clProgramVectorAdd");
+
+                log("Buffer size: " + bufSize + " bytes");
+
+                program = ctx.createProgramWithSource(kernelSrc);
+                devices = ctx.getContextInfo(WebCL.CL_CONTEXT_DEVICES);
+
+                program.buildProgram([devices[0]], ""); // TODO throws what?
+
+                // Create kernel and set arguments
+                var kernel = program.createKernel("ckVectorAdd");
+                kernel.setKernelArg(0, bufIn1);
+                kernel.setKernelArg(1, bufIn2);
+                kernel.setKernelArg(2, bufOut);
+                kernel.setKernelArg(3, vectorLength, WebCL.types.UINT);
+
+                // Create command queue using the first available device
+                var cmdQueue = ctx.createCommandQueue(devices[0], 0);
+
+                // Write the buffer to OpenCL device memory
+                cmdQueue.enqueueWriteBuffer(bufIn1, false, 0, bufSize, UIvec1, []);
+                cmdQueue.enqueueWriteBuffer(bufIn2, false, 0, bufSize, UIvec2, []);
+
+                // Init ND-range
+                var localWS = [8],
+                    globalWS = [Math.ceil(vectorLength / localWS) * localWS];
+
+                log("Global work item size: " + globalWS);
+                log("Local work item size: " + localWS);
+
+                // Execute (enqueue) kernel
+                cmdQueue.enqueueNDRangeKernel(kernel, globalWS.length, [], globalWS, localWS, []);
+
+                // Read the result buffer from OpenCL device
+                var resBuffer = new Uint32Array(vectorLength);
+                cmdQueue.enqueueReadBuffer(bufOut, false, 0, bufSize, resBuffer, []);
+                cmdQueue.finish(); //Finish all the operations
+
+                return resBuffer;
+
+            } catch (e) {
+                return e.message + "\n" +
+                    program.getProgramBuildInfo(devices[0], WebCL.CL_PROGRAM_BUILD_STATUS) + ":  " +
+                    program.getProgramBuildInfo(devices[0], WebCL.CL_PROGRAM_BUILD_LOG);
+            }
+        }
+
         return {
-            "webClInfo": webClInfo
+            "webClInfo": webClInfo,
+            "CL_vectorAdd": CL_vectorAdd
         }
     })
 );
