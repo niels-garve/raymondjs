98a9be93a78c1f1cca0502d4aacfcbb618c97fba
Niels Garve <niels.garve@yahoo.de>
2013-11-14 22:20:33 +0100

    refactored main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@
  *
  */
 
-
 /**
  * RequireJS alias/path configuration (http://requirejs.org/)
  *
@@ -12,14 +11,7 @@
  * @author Niels Garve, niels.garve.yahoo.de
  */
 requirejs.config({
-    shim: {
-        'webgl-obj-loader': {
-            deps: ['jquery']
-        }
-    },
-
     paths: {
-        // jquery library
         jquery: [
             // try content delivery network location first
             'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
@@ -27,13 +19,24 @@ requirejs.config({
             'lib/jquery-1.7.2.min'
         ],
 
-        // gl-matrix library
-        'gl-matrix': 'lib/gl-matrix-1.3.7',
+        // require.js plugins
+        domReady: 'lib/domReady',
+        text: 'lib/text',
+
+        'gl-matrix': 'lib/gl-matrix-1.3.7.min',
 
         'webgl-obj-loader': 'loaders/webgl-obj-loader',
 
         Stage: 'models/Stage'
-    }
+    },
+
+    shim: {
+        'webgl-obj-loader': {
+            deps: ['jquery']
+        }
+    },
+
+    bDebugMode: true
 });
 
 /*
@@ -43,96 +46,100 @@ requirejs.config({
  *
  */
 
-/* requireJS module definition */
-define(["jquery", "gl-matrix", "webgl-debug", "scene", "animation", "scene_explorer", "html_controller" ], 
-       (function($, glmatrix, WebGLDebugUtils, Scene, Animation, SceneExplorer, HtmlController ) {
+define([
+    'domReady',
+    'gl-matrix',
+    'webgl-debug',
+    'scene',
+    'animation',
+    'scene_explorer',
+    'html_controller'
+], function(domReady, glmatrix, WebGLDebugUtils, Scene, Animation, SceneExplorer, HtmlController) {
 
-        "use strict";
+    'use strict';
 
-        /**
-         * This function asks the HTML Canvas element with id canvas_name to create
-         * a context object for WebGL rendering.
-         *
-         * It also creates awrapper around it for debugging
-         * purposes, using webgl-debug.js
-         *
-         * @param canvas_name
-         * @returns {*}
-         */
-        var makeWebGLContext = function (canvas_name) {
-            // get the canvas element to be used for drawing
-            var canvas = $("#" + canvas_name).get(0);
-            if (!canvas) {
-                throw "HTML element with id '" + canvas_name + "' not found";
-            }
+    /**
+     * This function asks the HTML Canvas element with id canvasName to create
+     * a context object for WebGL rendering.
+     *
+     * It also creates awrapper around it for debugging
+     * purposes, using webgl-debug.js
+     *
+     * @param canvasName
+     * @returns {*}
+     */
+    var makeWebGLContext = function(canvasName) {
+        // get the canvas element to be used for drawing
+        var canvas = document.getElementById(canvasName);
+        if(!canvas) {
+            throw 'HTML element with id "' + canvasName + '" not found';
+        }
 
-            // get WebGL rendering context for canvas element
-            var options = {alpha: true, depth: true, antialias: true},
-                gl = canvas.getContext("webgl", options) || canvas.getContext("experimental-webgl", options);
+        // get WebGL rendering context for canvas element
+        var options = {alpha: true, depth: true, antialias: true},
+            gl = canvas.getContext('webgl', options) || canvas.getContext('experimental-webgl', options);
 
-            if (!gl) {
-                throw "could not create WebGL rendering context";
-            }
+        if(!gl) {
+            throw 'could not create WebGL rendering context';
+        }
 
-            // create a debugging wrapper of the context object
-/*
-            var throwOnGLError = function (err, funcName, args) {
-                throw WebGLDebugUtils.glEnumToString(err) + " was caused by call to: " + funcName;
+        // create a debugging wrapper of the context object
+        if(require.s.contexts._.config.bDebugMode) {
+            var throwOnGLError = function(err, funcName, args) {
+                throw WebGLDebugUtils.glEnumToString(err) + ' was caused by call to: ' + funcName;
             };
 
             gl = WebGLDebugUtils.makeDebugContext(gl, throwOnGLError);
-*/
-            return gl;
-        };
+        }
+        return gl;
+    };
 
-        /**
-         * create an animation for timing and regular calls of Scene.draw()
-         *
-         * @author Niels Garve, niels.garve.yahoo.de
-         * @param scene
-         * @returns {Animation}
-         */
-        var makeAnimation = function (scene) {
-            // create animation to rotate the scene
-            return new Animation((function (t) {
-                // (re-) draw the scene
-                scene.draw(t);
+    /**
+     * create an animation for timing and regular calls of Scene.draw()
+     *
+     * @author Niels Garve, niels.garve.yahoo.de
+     * @param scene
+     * @returns {Animation}
+     */
+    var makeAnimation = function(scene) {
+        // create animation to rotate the scene
+        return new Animation(function(t) {
+            // (re-) draw the scene
+            scene.draw(t);
 
-            })); // end animation callback
-        };
+        }); // end animation callback
+    };
 
-        /**
-         * main program, to be called once the document has loaded
-         * and the DOM has been constructed
-         *
-         * @author Hartmut Schirmacher, hschirmacher.beuth-hochschule.de
-         * @author Niels Garve, niels.garve.yahoo.de
-         */
-        $(document).ready((function () {
-            // catch errors for debugging purposes
-            try {
-                log("document ready - starting!");
+    /**
+     * main program, to be called once the document has loaded
+     * and the DOM has been constructed
+     *
+     * @author Hartmut Schirmacher, hschirmacher.beuth-hochschule.de
+     * @author Niels Garve, niels.garve.yahoo.de
+     */
+    domReady(function() {
+        // catch errors for debugging purposes
+        try {
+            // create WebGL context object for the named canvas object
+            var gl = makeWebGLContext('drawing_area'),
+            // create scene and animation, and start drawing
+                scene = new Scene(gl),
+                animation = makeAnimation(scene); // do not start yet
 
-                // create WebGL context object for the named canvas object
-                var gl = makeWebGLContext("drawing_area"),
-                // create scene and animation, and start drawing
-                    scene = new Scene(gl),
-                    animation = makeAnimation(scene), // do not start yet
-                    explorer = new SceneExplorer(gl.canvas, false, scene),
-                // create HTML controller that handles all the interaction of
-                // HTML elements with the scene and the animation
-                    controller = new HtmlController(scene, animation);
+            new SceneExplorer(gl.canvas, false, scene);
+            // create HTML controller that handles all the interaction of
+            // HTML elements with the scene and the animation
+            new HtmlController(scene, animation);
 
-                scene.draw(0.0);
-            } catch (err) {
-                var $error = $("#error");
-                if ($error) {
-                    $error.text(err.message || err);
-                    $error.css('display', 'block');
-                }
-                log("exception: " + (err.message || err));
-                throw err;
+            scene.draw(0.0);
+        } catch (err) {
+            var errorElement = document.getElementById('error');
+            if(errorElement) {
+                errorElement.innerHTML = err.message || err;
+                errorElement.style.display = 'block';
             }
-        })); // $(document).ready()
-    }) // self executing function
-); // define module
+            log('exception: ' + (err.message || err));
+            throw err;
+        }
+    });
+}); // define module
