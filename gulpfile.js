bd5a60b48824c8e1c558db3193b5a0febb900781
Niels Garve <niels.garve@mailbox.org>
2015-04-19 20:04:18 +0200

    - ported engine to node.js - implemented browserify gulp task, so it can still be a frontend engine

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,50 @@
-var gulp = require('gulp'),
-    spawn = require('child_process').spawn,
-    browserSync = require('browser-sync');
+'use strict';
 
-gulp.task('rjs', function(done) {
-    spawn('r.js', ['-o', 'build.js'], {stdio: 'inherit'})
-        .on('close', done);
-});
+var watchify = require('watchify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var assign = require('lodash.assign');
+var connect = require('gulp-connect');
 
-gulp.task('rjs-copy', ['rjs'], function() {
-    return gulp.src([
-        'dist/raymond.js',
-        'dist/raymond.js.map'
-    ])
-        .pipe(gulp.dest('example'));
-});
+// add custom browserify options here
+var customOpts = {
+    entries: [ './server/engine.js' ],
+    debug: true,
+    standalone: 'Raymond'
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
 
-gulp.task('watch',/* ['rjs-copy'],*/ function() {
-    browserSync({
-        server: {
-            baseDir: '.'
-        },
-        open: false
+b.transform('brfs');
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+    return b.bundle()
+        // log errors if they happen
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('server/engine.js'))
+        // optional, remove if you don't need to buffer file contents
+        .pipe(buffer())
+        // optional, remove if you dont want sourcemaps
+        .pipe(sourcemaps.init({ loadMaps: true })) // loads map from browserify file
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./')) // writes .map file
+        .pipe(gulp.dest('./client'))
+        .pipe(connect.reload());
+}
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'client',
+        livereload: true
     });
-
-    //gulp.watch(['example/index.html', 'example/example.js', 'example/raymond.js'], browserSync.reload);
-    //gulp.watch(['dev/**/*.js'], ['rjs-copy']);
 });
-
-gulp.task('default', ['watch']);
-gulp.task('build', ['rjs']);
